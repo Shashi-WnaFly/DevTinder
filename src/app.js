1407ca5154be58915621b97db6da6c808962fd92
Shashi-Anand <shashianand2600@gmail.com>
2025-03-28 17:19:53 +0530

    exploring routes and regex

--- src/app.js
+++ src/app.js
@@ -2,21 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Hello World");
+// app.use("/hello", (req, res) => {
+//   res.send("Hello World");
+// });
+
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
+  res.send({firstName: "Shashi", lastName: "Anand"});
 });
 
-app.get("/user", (req, res) => {
-  res.send({firstName: "Shashi", secondName: "Anand"});
-});
+// app.delete("/user", (req, res) => {
+//   res.send("Data is deleted successfully from the database.");
+// });
 
-app.delete("/user", (req, res) => {
-  res.send("Data is deleted successfully from the database.");
-});
-
-app.post("/user", (req, res) => {
-  res.send("Data saved successfully to the database.");
-})
+// app.post("/user", (req, res) => {
+//   res.send("Data saved successfully to the database.");
+// })
 
 app.listen(7777, () => {
   console.log("Server is successfully running....");
