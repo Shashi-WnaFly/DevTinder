9c1659dce60827a188f6688e2d061e35ebfee6bf
Shashi-Anand <shashianand2600@gmail.com>
2025-03-29 21:39:27 +0530

    installing mongoose and learn wild card error handling

--- src/app.js
+++ src/app.js
@@ -2,17 +2,27 @@ const express = require("express");
 const {adminAuth} = require("./middlewares/auth");
 const app = express();
 
-app.use("/admin", adminAuth);
+app.use("/", (err, req, res, next) => {
+  if(err){
+    res.send("Something is wrong contact support team.");
+  }
+})
 
-app.get("/admin/getData", (req, res) => {
-  res.send("All the admin data");
-});
-app.get("/admin/updatedata", (req, res) => {
-  res.send("All the admin data updated");
-});
-app.get("/admin/removedata", (req, res) => {
-  res.send("All the admin data removed");
-});
+app.get("/getUserData", (req, res) => {
+  try{
+    // throw new Error("viral photo");
+    res.send("All the user Data");
+  }
+  catch(err){
+    res.send("Something is wrong");
+  }
+})
+
+// app.use("/", (err, req, res, next) => {
+//   if(err){
+//     res.send("Something is wrong contact support team.");
+//   }
+// })
 
 app.listen(7777, () => {
   console.log("Server is successfully running....");
