864d4eabeed682c7e6b5e060a5539a9dff139eb2
Shashi-Anand <shashianand2600@gmail.com>
2025-04-02 18:33:12 +0530

    create a database and insert data into it

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+
+  const userObj = new User({
+    firstName: "Shashi",
+    lastName: "Anand",
+    emailId: "shashianand@goto.com",
+    age: 28,
+  });
+
+  await userObj.save();
+  res.send("User created successfully.");
+})
 
 connectDB()
   .then(() => {
