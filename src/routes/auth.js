9078bc4f095c51a67290882ef96a6d3bdb20b0dd
Shashi-Anand <shashianand2600@gmail.com>
2025-04-11 22:01:54 +0530

    create a logout api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -39,30 +39,31 @@ router.post("/signup", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-    try {
-      const { emailId, password } = req.body;
-  
-      if (!validator.isEmail(emailId)) 
-        throw new Error("Invalid credentials");
-  
-      const user = await User.findOne({ emailId: emailId });
-  
-      if (!user) 
-        throw new Error("Invalid credentials");
-  
-      const isPasswordMatch = await user.validatePassword(password);
-  
-      if (isPasswordMatch) {
-        const token = await user.getJWT();
-        res.cookie("token", token, { expires : new Date(Date.now() + 9000000)});
-        res.send("login successfully.");
-      }
-      else 
-        throw new Error("Invalid credentials");
-    } 
-    catch (err) {
-      res.status(400).send("ERROR : " + err.message);
-    }
+  try {
+    const { emailId, password } = req.body;
+
+    if (!validator.isEmail(emailId)) throw new Error("Invalid credentials");
+
+    const user = await User.findOne({ emailId: emailId });
+
+    if (!user) throw new Error("Invalid credentials");
+
+    const isPasswordMatch = await user.validatePassword(password);
+
+    if (isPasswordMatch) {
+      const token = await user.getJWT();
+      res.cookie("token", token, { expires: new Date(Date.now() + 9000000) });
+      res.send("login successfully.");
+    } else throw new Error("Invalid credentials");
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  res
+    .cookie("token", null, { expires: new Date(Date.now()) })
+    .send("logout successful");
 });
 
 module.exports = router;
