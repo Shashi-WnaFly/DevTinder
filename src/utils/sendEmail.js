01a455df735a4c960cf0cac3e6902bda19367490
Shashi-Anand <shashianand2600@gmail.com>
2025-09-06 21:01:34 +0530

    ses set  up configuration

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,59 +1,54 @@
-var AWS = require("aws-sdk");
-// Set the region
-AWS.config.update({ region: "ap-south-1" });
+const { SendEmailCommand } = require("@aws-sdk/client-ses");
+const { sesClient } = require("./sesClient");
 
-// Create sendEmail params
-var params = {
-  Destination: {
-    /* required */
-    CcAddresses: [
-      "EMAIL_ADDRESS",
-      /* more items */
-      "shashi@tinderdev.club"
-    ],
-    ToAddresses: [
-        "EMAIL_ADDRESS",
-        /* more items */
-        "shshianand2600@gmail.com"
-        
-    ],
-  },
-  Message: {
-    /* required */
-    Body: {
-      /* required */
-      Html: {
-        Charset: "UTF-8",
-        Data: <h1>This is the body of ses email</h1>,
+const createSendEmailCommand = (toAddress, fromAddress) => {
+  return new SendEmailCommand({
+    Destination: {
+      CcAddresses: [
+      ],
+      ToAddresses: [
+        toAddress,
+      ],
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: "<h1>This is the Body of the email</h1>",
+        },
+        Text: {
+          Charset: "UTF-8",
+          Data: "Welcome, to our devtinder family.",
+        },
       },
-      Text: {
+      Subject: {
         Charset: "UTF-8",
-        Data: "Hey! Now u r our client. Please any thing you feel feedback us sir/madam",
+        Data: "devtinder message or email testing",
       },
     },
-    Subject: {
-      Charset: "UTF-8",
-      Data: "Test email",
-    },
-  },
-  Source: "SENDER_EMAIL_ADDRESS" /* required */,
-  ReplyToAddresses: [
-    "EMAIL_ADDRESS",
-    /* more items */
-  ],
+    Source: fromAddress,
+    ReplyToAddresses: [
+    ],
+  });
 };
 
-// Create the promise and SES service object
-var sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
-  .sendEmail(params)
-  .promise();
+const run = async () => {
+  const sendEmailCommand = createSendEmailCommand(
+    "shashianand2600@gmail.com",
+    "shashi@tinderdev.club"
+  );
 
-// Handle promise's fulfilled/rejected states
-sendPromise
-  .then(function (data) {
-    console.log(data.MessageId);
-  })
-  .catch(function (err) {
-    console.error(err, err.stack);
-  });
-// snippet-end:[ses.JavaScript.email.sendEmail]
+  try {
+    return await sesClient.send(sendEmailCommand);
+  } catch (caught) {
+    if (caught instanceof Error && caught.name === "MessageRejected") {
+      /** @type { import('@aws-sdk/client-ses').MessageRejected} */
+      const messageRejectedError = caught;
+      return messageRejectedError;
+    }
+    throw caught;
+  }
+};
+
+// snippet-end:[ses.JavaScript.email.sendEmailV3]
+module.exports = { run };
